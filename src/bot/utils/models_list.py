e73703e6b98595fd364634b407a7316853da8a5b
michaelik1 <gulkov.mikhail@mail.ru>
2026-02-14 11:27:11 +0300

    Add utils models_list.py

---
+++ src/bot/utils/models_list.py
@@ -0,0 +1,13 @@
+models_dict = {
+    "LLaMA-8b": "llama8b",
+    "LLaMA-70b": "llama70b",
+    "LLaMA-405b": "llama405b",
+    "Mistral-7b": "mistral7b",
+    "Gemma-7b": "gemma7b",
+    "Arctic": "nemotron340b",
+    "Nemotron-340b": "arctic",
+    "DeepSeek-v3": "phi3mini",
+    "Phi-3mini": "deepseekv3",
+    "Qwen-3coder": "qwen3coder",
+    "Kimi-2.5": "kimi2.5"
+}
