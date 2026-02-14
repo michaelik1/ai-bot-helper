743d6531c272f09ed9e128e1547081d41f7edf95
michaelik1 <gulkov.mikhail@mail.ru>
2026-02-14 14:59:53 +0300

    Add rules_and_help handler

--- src/main.py
+++ src/main.py
@@ -5,13 +5,14 @@ from os import getenv
 from src.bot.handlers.startup import handler_startup
 from src.bot.handlers.models import handler_models
 from src.bot.handlers.profile import handler_profile
+from src.bot.handlers.rules_and_help import handler_rules
 from src.bot.services.user_manager import UserManager
 
 async def main():
     bot_key = getenv("TGBOT_KEY")
     bot = Bot(token=bot_key)
     dp = Dispatcher()
-    dp.include_routers(handler_startup, handler_models, handler_profile)
+    dp.include_routers(handler_startup, handler_models, handler_profile, handler_rules)
     UserManager.setup()
     await dp.start_polling(bot)
 
